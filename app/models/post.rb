2c839b659012b4261d5c237e1a61288f9f52fae2
David Mauricio Gomez Zapata <davidmaogomezz@gmail.com>
2021-10-02 13:27:31 -0500

    Validation content and title model post

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,4 +13,13 @@ class Post < ApplicationRecord
   has_many :category_posts, dependent: :restrict_with_exception
   has_many :user_posts, dependent: :restrict_with_exception
   has_many :comments, dependent: :restrict_with_exception
+
+  # VALIDATIONS
+  validates :title, length: { minimum: 3,
+                              too_short: '
+                                The post title must be at least 3 characters long' }
+
+  validates :content, length: { minimum: 200,
+                                too_short: '
+                                  The post content must be at least 200 characters long' }
 end
