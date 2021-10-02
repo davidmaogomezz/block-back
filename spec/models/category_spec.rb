18c65668a19686d0e5bede8e89e81fb0e517b452
David Mauricio Gomez Zapata <davidmaogomezz@gmail.com>
2021-10-02 12:59:40 -0500

    Create tests model category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -15,5 +15,26 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:name) { Faker::String.random(length: 3..12) }
+
+  it 'With attributes valid of factory' do
+    expect(FactoryBot.create(:category)).to be_valid
+  end
+
+  it 'With attributes valid' do
+    expect(FactoryBot.create(:category, user: user, name: name)).to be_valid
+  end
+
+  it 'With attribute user nil' do
+    expect(Category.new(user: nil, name: name)).not_to be_valid
+  end
+
+  it 'With attribute name blank' do
+    expect(Category.new(user: user, name: '')).not_to be_valid
+  end
+
+  it 'With attribute name lenght 2 characters' do
+    expect(Category.new(user: user, name: Faker::String.random(length: 2))).not_to be_valid
+  end
 end
