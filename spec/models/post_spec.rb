5ce59d2d13d45a03e17a7bdbcdfd5be87b6a1e5b
David Mauricio Gomez Zapata <davidmaogomezz@gmail.com>
2021-10-02 13:27:58 -0500

    Create test for model post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,5 +11,22 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:title) { Faker::String.random(length: 3) }
+  let(:content) { Faker::String.random(length: 200) }
+
+  it 'With attributes valid of factory' do
+    expect(FactoryBot.create(:post)).to be_valid
+  end
+
+  it 'With attributes valid' do
+    expect(FactoryBot.create(:post, title: title, content: content)).to be_valid
+  end
+
+  it 'With attribute title nil' do
+    expect(Post.new(title: nil, content: content)).not_to be_valid
+  end
+
+  it 'With attribute content nil' do
+    expect(Post.new(title: title, content: nil)).not_to be_valid
+  end
 end
