0c0df68b3cc531276779ced840a69dbeaad6a5fb
David Mauricio Gomez Zapata <davidmaogomezz@gmail.com>
2021-10-02 12:58:54 -0500

    validations name category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ApplicationRecord
   # RELATIONS
   has_many :category_posts, dependent: :restrict_with_exception
   belongs_to :user
+
+  # VALIDATIONS
+  validates :name, length: { minimum: 3,
+                             too_short: 'The category name must be at least 3 characters long' }
 end
